c5cf9a37b68aabfb62c9e3b9464d642efc93b771
AsgarovElchin <elcinaskerovki@gmail.com>
2025-03-29 14:13:12 +0400

    add trailing search icon with focus-based tint in FilterBar

--- app/src/main/java/asgarov/elchin/plantly/feature_explore/presentation/NameFilter.kt
+++ app/src/main/java/asgarov/elchin/plantly/feature_explore/presentation/NameFilter.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -16,6 +19,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.onFocusChanged
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
@@ -34,11 +39,16 @@ fun FilterBar(
     var query by remember { mutableStateOf(searchQuery) }
     val coroutineScope = rememberCoroutineScope()
     var debounceJob by remember { mutableStateOf<Job?>(null) }
+    var isFocused by remember { mutableStateOf(false) }
+
 
     Column(
         modifier = Modifier
             .fillMaxWidth()
             .padding(12.dp)
+            .onFocusChanged { focusState ->
+                isFocused = focusState.isFocused
+            }
     ) {
         OutlinedTextField(
             value = query,
@@ -54,6 +64,14 @@ fun FilterBar(
             keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text),
             maxLines = 1,
             shape = RoundedCornerShape(16.dp),
+            trailingIcon = {
+                if (query.isBlank()){
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Search Icon",
+                    tint = if (isFocused) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurfaceVariant
+                )
+            }},
             colors = OutlinedTextFieldDefaults.colors(
                 focusedBorderColor = MaterialTheme.colorScheme.primary,
                 focusedLabelColor = MaterialTheme.colorScheme.primary
